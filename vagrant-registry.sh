fc5b3f6dacdba7a030aeeda4e6d1a012eeb39b44
MarkMoudy <mark.moudy@ericsson.com>
2014-05-02 14:30:09 -0500

    increased the timeout for the registry push

--- vagrant-registry.sh
+++ vagrant-registry.sh
@@ -8,7 +8,7 @@ cd /home/core/share/Dockerfiles/Docker-Registry && /usr/bin/docker build --rm -t
 /usr/bin/docker run -d -p 5000:5000 -v /home/core/share/config/vendor/docker-registry:/registry-conf -e DOCKER_REGISTRY_CONFIG=/registry-conf/config.yml 192.168.2.90:5000/moudy/registry
 
 # wait for registry to come up
-echo "Waiting 3 seconds for the Registry to come up"
-sleep 3s
+echo "Waiting 5 seconds for the Registry to come up"
+sleep 5s
 # push image to Registry
 /usr/bin/docker push 192.168.2.90:5000/moudy/registry
