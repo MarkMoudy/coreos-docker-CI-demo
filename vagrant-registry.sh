4d06e7c3816956aa968cc4cc417e2d943f118e92
MarkMoudy <mark.moudy@ericsson.com>
2014-05-02 14:32:03 -0500

    fixed registry not starting issue and changed alias setting

--- vagrant-registry.sh
+++ vagrant-registry.sh
@@ -1,10 +1,11 @@
 ## Vagrant shell provisioner to build the Docker Private Registry image and start the registry 
 ## Set up useful Docker alias commands 
-echo "alias dl='docker ps -l -q'"
-echo "alias cdel='docker rm $(docker ps -a -q)'" 
+alias dl='docker ps -l -q'
+alias cdel='docker rm $(docker ps -a -q)'
 
 ## build docker registry image
-cd /home/core/share/Dockerfiles/Docker-Registry && /usr/bin/docker build --rm -t 192.168.2.90:5000/moudy/registry .
+cd /home/core/share/Dockerfiles/Docker-Registry && \
+/usr/bin/docker build --rm -t 192.168.2.90:5000/moudy/registry . && \
 /usr/bin/docker run -d -p 5000:5000 -v /home/core/share/config/vendor/docker-registry:/registry-conf -e DOCKER_REGISTRY_CONFIG=/registry-conf/config.yml 192.168.2.90:5000/moudy/registry
 
 # wait for registry to come up
