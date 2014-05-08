c6bc01c707414b6defa9379c850d32ae89a19f0a
MarkMoudy <mark.moudy@ericsson.com>
2014-05-08 14:50:13 -0500

    added screencast demo for fleet scheduling

---
+++ screencast-demo/build-screencast-demo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /home/core/share/screencast-demo/static-web-1 && \
+docker build --rm -t 192.168.2.90:5000/demo/static-web-1 . && \
+docker push 192.168.2.90:5000/demo/static-web-1
+
+cd /home/core/share/screencast-demo/static-web-2 && \
+docker build --rm -t 192.168.2.90:5000/demo/static-web-2 . && \
+docker push 192.168.2.90:5000/demo/static-web-2
+
+cd /home/core/share/screencast-demo/static-web-3 && \
+docker build --rm -t 192.168.2.90:5000/demo/static-web-3 . && \
+docker push 192.168.2.90:5000/demo/static-web-3
