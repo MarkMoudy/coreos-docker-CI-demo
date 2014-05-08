f879e6ae551b0348b8f1415abcd5b5664ec606ba
MarkMoudy <mark.moudy@ericsson.com>
2014-05-08 14:46:56 -0500

    updated docker registry to persist images after container stops

--- vagrant-registry.sh
+++ vagrant-registry.sh
@@ -6,10 +6,5 @@ alias cdel='docker rm $(docker ps -a -q)'
 ## build docker registry image
 cd /home/core/share/Dockerfiles/Docker-Registry && \
 /usr/bin/docker build --rm -t 192.168.2.90:5000/moudy/registry . && \
-/usr/bin/docker run -d -p 5000:5000 -v /home/core/share/config/vendor/docker-registry:/registry-conf -e DOCKER_REGISTRY_CONFIG=/registry-conf/config.yml 192.168.2.90:5000/moudy/registry
-
-# wait for registry to come up
-echo "Waiting 5 seconds for the Registry to come up"
-sleep 5s
-# push image to Registry
-/usr/bin/docker push 192.168.2.90:5000/moudy/registry
+/usr/bin/docker run -d -p 5000:5000 -v /home/core/share/config/vendor/docker-registry:/registry-conf -v /tmp/registry:/tmp/registry -e DOCKER_REGISTRY_CONFIG=/registry-conf/config.yml 192.168.2.90:5000/moudy/registry && \
+echo "Waiting 5 seconds for the Registry to come up";sleep 5s;/usr/bin/docker push 192.168.2.90:5000/moudy/registry
