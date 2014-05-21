e0ea86bc8beef83bea303241471e20076bf3415d
MarkMoudy <mark.moudy@ericsson.com>
2014-05-21 13:28:11 -0500

    updated docker registry init to copy registry config to host

--- vagrant-registry.sh
+++ vagrant-registry.sh
@@ -7,5 +7,7 @@ alias cdel='docker rm $(docker ps -a -q)'
 cd /home/core/share/Dockerfiles/Docker-Registry && \
 /usr/bin/docker build --rm -t 192.168.2.90:5000/moudy/registry . && \
 mkdir /registry && \
-/usr/bin/docker run -d -p 5000:5000 -v /home/core/share/config/vendor/docker-registry:/registry-conf -v /registry:/registry:rw -e DOCKER_REGISTRY_CONFIG=/registry-conf/config.yml 192.168.2.90:5000/moudy/registry && \
+mkdir /home/core/registry-conf && \
+cp /home/core/share/config/vendor/docker-registry/config.yml /home/core/registry-conf/config.yml
+/usr/bin/docker run -d -p 5000:5000 -v /home/core/registry-conf:/registry-conf -v /registry:/registry:rw -e DOCKER_REGISTRY_CONFIG=/registry-conf/config.yml 192.168.2.90:5000/moudy/registry && \
 echo "Waiting for the Registry to come up";sleep 5s;curl -X GET http://192.168.2.90:5000/v1/_ping;/usr/bin/docker push 192.168.2.90:5000/moudy/registry
